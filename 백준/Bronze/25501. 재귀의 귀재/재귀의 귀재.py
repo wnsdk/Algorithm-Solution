a08ba5f1b032f1f8bb6f5bf4043a9b22be56394d
wnsdk <69608468+wnsdk@users.noreply.github.com>
2023-01-06 10:41:02 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 164 ms, Memory: 115864 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
@@ -0,0 +1,23 @@
+T = int(input())
+ans = ''
+
+for _ in range(T):
+    cnt = 0
+    def recursion(s, l, r):
+        global cnt
+        cnt += 1
+
+        if l >= r:
+            return 1
+        elif s[l] != s[r]:
+            return 0
+        else:
+            return recursion(s, l + 1, r - 1)
+
+
+    def isPalindrome(s):
+        return recursion(s, 0, len(s) - 1)
+
+    ans += str(isPalindrome(input())) + ' ' + str(cnt) + '\n'
+
+print(ans)
