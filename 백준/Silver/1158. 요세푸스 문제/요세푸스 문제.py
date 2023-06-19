9185fd2ee8bcb01d5fc10b7dc6822b5c1e48e256
Juna Choi <jn307742@gmail.com>
2023-06-19 22:17:30 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,20 +1,12 @@
-from collections import deque
-
 n, k = map(int, input().split())
-ans = '<'
 
-q = deque([i for i in range(1, n + 1)])
+arr = [i for i in range(1, n + 1)]
 
-for i in range(n):
-    for j in range(k):
-        num = q.popleft()
-        if j == k - 1:
-            ans += str(num)
-        else:
-            q.append(num)
+print('<', end='')
 
-    if i != n - 1:
-        ans += ', '
+i = 0
+for _ in range(n - 1):
+    i = (i + k - 1) % len(arr)
+    print(arr.pop(i), end=', ')
 
-ans += '>'
-print(ans)
+print(arr.pop(), end='>')
