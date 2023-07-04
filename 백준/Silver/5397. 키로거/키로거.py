6bf35866d5e76b05e23e875187ea5b8a9858f35e
Juna Choi <jn307742@gmail.com>
2023-07-04 17:56:30 +0900

    [Silver II] Title: 키로거, Time: 1100 ms, Memory: 46888 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.py
+++ 백준/Silver/5397. 키로거/키로거.py
@@ -1,18 +1,29 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
 for _ in range(int(input())):
-    ans_l = deque()
-    ans_r = deque()
-    for cmd in input():
-        if cmd == '-':
-            if len(ans_l):
-                ans_l.pop()
-        elif cmd == '<':
-            if len(ans_l):
-                ans_r.appendleft(ans_l.pop())
-        elif cmd == '>':
-            if len(ans_r):
-                ans_l.append(ans_r.popleft())
+    front = deque()
+    back = deque()
+
+    for command in list(input().strip()):
+        # 지우기
+        if command == '-':
+            if front:
+                front.pop()
+
+        # 커서 왼쪽 이동
+        elif command == '<':
+            if front:
+                back.appendleft(front.pop())
+
+        # 커서 오른쪽 이동
+        elif command == '>':
+            if back:
+                front.append((back.popleft()))
+
+        # 글자 입력
         else:
-            ans_l.append(cmd)
-    print(''.join(ans_l) + ''.join(ans_r))
+            front.append(command)
+
+    print(''.join(front) + ''.join(back))
