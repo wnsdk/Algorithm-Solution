bd74ad46fc59d4e0e715fb039d49ceb37202d5a1
Juna Choi <jn307742@gmail.com>
2024-01-05 18:52:12 +0900

    [Silver III] Title: N과 M (1), Time: 144 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,19 +1,7 @@
-a, b = map(int, input().split())
-nums = []
-chk = [False] * (a + 1)
+from itertools import permutations
 
+n, r = map(int, input().split())
+arr = [i for i in range(1, n + 1)]
 
-def permutation(n, r):
-    if not r:
-        print(*nums)
-
-    for i in range(1, n + 1):
-        if not chk[i]:
-            chk[i] = True
-            nums.append(i)
-            permutation(n, r - 1)
-            nums.pop()
-            chk[i] = False
-
-
-permutation(a, b)
+for ans in permutations(arr, r):
+    print(*ans)
