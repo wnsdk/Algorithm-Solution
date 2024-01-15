0e9d2f53db53de4ed2cc477172217ed4c3b43e76
Juna Choi <jn307742@gmail.com>
2024-01-15 09:50:22 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,6 +1,10 @@
 n = int(input())
-dp = [0, 1]
-for i in range(2, n + 1):
-    dp.append(dp[i - 1] + dp[i - 2])
 
-print(dp[n])
+cache = [-1] * (n + 1)
+cache[0] = 0
+cache[1] = 1
+
+for i in range(2, n + 1):
+    cache[i] = cache[i - 1] + cache[i - 2]
+
+print(cache[n])
