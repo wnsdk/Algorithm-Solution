da29019f868010240d4c50e092fffb4b60bd9b4e
Juna Choi <jn307742@gmail.com>
2024-01-24 08:29:54 +0900

    [Bronze II] Title: 소수 찾기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,17 @@
+def is_prime(x):
+    if x < 2:
+        return False
+    
+    for a in range(2, x):
+        if x % a == 0:
+            return False
+    return True
+
+
+input()
+ans = 0
+for n in map(int, input().split()):
+    if is_prime(n):
+        ans += 1
+
+print(ans)
