76959fced183b99246a5c33ee59f26247d37eb9e
Juna Choi <jn307742@gmail.com>
2024-01-26 10:27:19 +0900

    [Gold V] Title: 집합의 표현, Time: 320 ms, Memory: 76584 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -3,15 +3,21 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10 ** 6)
 
 
+# 두 노드의 루트 노드를 일치시키기 (둘 중 작은 값으로)
 def union(x, y):
-    px = find(x)
-    py = find(y)
-    if px != py:
-        parent[px] = py
+    x = find(x)
+    y = find(y)
+    
+    if x < y:
+        parent[y] = x
+    else:
+        parent[x] = y
 
 
+# 노드 x의 루트 노드 찾기
 def find(x):
-    if x != parent[x]:
+    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀 호출
+    if parent[x] != x:
         parent[x] = find(parent[x])
     return parent[x]
 
