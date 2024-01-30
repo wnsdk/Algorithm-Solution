1ca045623c4f6b12a7cb7628e939083d93af184b
Juna Choi <jn307742@gmail.com>
2024-01-30 09:24:08 +0900

    [Gold IV] Title: 좋은수열, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2661. 좋은수열/좋은수열.py
+++ 백준/Gold/2661. 좋은수열/좋은수열.py
@@ -1,36 +1,19 @@
-N = int(input())
-ans = [0] * N
-ans[0] = 1
+def f(s):
+    # 나쁜 수열인지 확인하기
+    l = 1
+    while l <= len(s) // 2:
+        if s[len(s) - l * 2: len(s) - l] == s[len(s) - l: len(s)]:
+            return
+        l += 1
 
-# i번째 자리에 숫자를 넣겠다.
-def dfs(i):
-    global N
+    if len(s) == n:
+        print(s)
+        exit(0)
 
-    if i == N:
-        for num in ans:
-            print(num, end='')
-        exit()
+    f(s + '1')
+    f(s + '2')
+    f(s + '3')
 
-    for n in range(1, 4):
-        ans[i] = n
 
-        # 나쁜 수열인지 체크하기
-        isBad = False
-        for j in range((i + 1) // 2):
-            if ans[i - 2 * j - 1:i - j] == ans[i - j:i + 1]:
-                isBad = True
-                break
-
-        if isBad and n == 3:
-            return False
-
-        if isBad:
-            continue
-
-        if not dfs(i + 1):
-            continue
-
-    return True
-
-dfs(0)
-
+n = int(input())
+f('')
