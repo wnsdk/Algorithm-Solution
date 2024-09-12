083e1da9cd9485cd7f0664dcf9fafa6e628a1c30
Juna Choi <jn307742@gmail.com>
2024-09-12 20:35:49 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 928 ms, Memory: 105940 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -2,17 +2,15 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(n)]
+board = [list(map(int, input().split())) for _ in range(n)]
 
-prefix_sum = [[0] * (n + 1)] + [[0] * (n + 1) for _ in range(n + 1)]
-
-for y in range(1, n + 1):
-    for x in range(1, n + 1):
-        prefix_sum[y][x] = matrix[y - 1][x - 1] + prefix_sum[y][x - 1] + prefix_sum[y - 1][x] - prefix_sum[y - 1][x - 1]
+prefix_sum = [[0] * (n + 1) for _ in range(n + 1)]
+for y in range(n):
+    for x in range(n):
+        prefix_sum[y + 1][x + 1] = prefix_sum[y][x + 1] + prefix_sum[y + 1][x] - prefix_sum[y][x] + board[y][x]
 
 for _ in range(m):
     y1, x1, y2, x2 = map(int, input().split())
     x1 -= 1
     y1 -= 1
-
-    print(prefix_sum[y2][x2] - prefix_sum[y2][x1] - prefix_sum[y1][x2] + prefix_sum[y1][x1])
+    print(prefix_sum[y2][x2] - prefix_sum[y1][x2] - prefix_sum[y2][x1] + prefix_sum[y1][x1])
