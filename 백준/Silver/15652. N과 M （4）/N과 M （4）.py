388c9a5387998d0eabfa1333da7112255dccfec5
Juna Choi <jn307742@gmail.com>
2024-09-12 20:07:14 +0900

    [Silver III] Title: N과 M (4), Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,7 +1,15 @@
-from itertools import combinations_with_replacement
+N, M = map(int, input().split())
+ans = []
 
-n, r = map(int, input().split())
-arr = [i for i in range(1, n + 1)]
 
-for ans in combinations_with_replacement(arr, r):
-    print(*ans)
+def combinaction_with_replacement(n, m, idx):
+    if not m:
+        print(*ans)
+    else:
+        for i in range(idx, n + 1):
+            ans.append(i)
+            combinaction_with_replacement(n, m - 1, i)
+            ans.pop()
+
+
+combinaction_with_replacement(N, M, 1)
