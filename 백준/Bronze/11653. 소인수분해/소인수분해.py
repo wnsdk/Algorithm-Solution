cc309876e73014a6b218f5c7a28f7b40ac90086e
Juna Choi <jn307742@gmail.com>
2024-10-02 10:00:17 +0900

    [Bronze I] Title: 소인수분해, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,25 +1,19 @@
-# 10000000 이하의 수를 소인수분해
-# 10000000 ** 0.5 이하의 소수들을 이용해서 나눌 수 있는데 까지 나누기
-# 그러고 나서 남은 수는 자연스럽게 1 아니면 10000000 ** 0.5 이상의 소수가 됨
+n = int(input())
+m = int(n ** 0.5) + 1
 
-# 에라토스테네스의 체
-N = int(10000000 ** 0.5) + 1
-chk = [True] * (N + 1)
-for i in range(2, int(N ** 0.5) + 1):
-    if chk[i]:
-        for j in range(i + i, N, i):
-            chk[j] = False
+is_prime = [True] * (m + 1)
 
-prime = [i for i in range(2, N + 1) if chk[i]]
-N = int(input())
+for x in range(2, int(m ** 0.5) + 1):
+    if is_prime[x]:
+        for y in range(x * 2, m + 1, x):
+            is_prime[y] = False
 
-if N != 1:
-    i = 0
-    while i < len(prime) and N >= prime[i] ** 2:
-        if not(N % prime[i]):
-            N //= prime[i]
-            print(prime[i])
-        else:
-            i += 1
-    if N != 1:
-        print(N)
+prime = [x for x in range(2, m + 1) if is_prime[x]]
+
+for p in prime:
+    while n % p == 0:
+        n //= p
+        print(p)
+
+if n > 1:
+    print(n)
