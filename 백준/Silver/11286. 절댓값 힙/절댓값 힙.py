97edcfa4cbedce5bf80f64a94c2085299c0c2055
Juna Choi <jn307742@gmail.com>
2024-10-02 10:14:07 +0900

    [Silver I] Title: 절댓값 힙, Time: 104 ms, Memory: 35724 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,27 +1,23 @@
-import heapq
-import sys
-
+import sys, heapq
 input = sys.stdin.readline
-h1 = [] # 양수
-h2 = [] # 음수
+h1 = [] # 양수만 저장(최솟값 힙)
+h2 = [] # 음수만 저장(최댓값 힙)
 
 for _ in range(int(input())):
-    x = int(input())
-
-    # 삽입 연산
-    if x:
-        if x > 0:
-            heapq.heappush(h1, x)
-        else:
-            heapq.heappush(h2, -x)
-            
-    # 삭제 연산
+    cmd = int(input())
+    if cmd > 0:
+        heapq.heappush(h1, cmd)
+    elif cmd < 0:
+        heapq.heappush(h2, -cmd)
     else:
         if not h1 and not h2:
             print(0)
-        elif not h1:
+        elif not h1 and h2:
             print(-heapq.heappop(h2))
-        elif not h2:
+        elif h1 and not h2:
             print(heapq.heappop(h1))
         else:
-            print(heapq.heappop(h1) if h1[0] < h2[0] else -heapq.heappop(h2))
+            if h1[0] >= h2[0]:
+                print(-heapq.heappop(h2))
+            else:
+                print(heapq.heappop(h1))
