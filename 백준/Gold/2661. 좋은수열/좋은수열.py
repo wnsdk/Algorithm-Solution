b282c69468af51dd0a9f16d59dd775fb3d6891e4
Juna Choi <jn307742@gmail.com>
2024-10-02 11:51:53 +0900

    [Gold IV] Title: 좋은수열, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2661. 좋은수열/좋은수열.py
+++ 백준/Gold/2661. 좋은수열/좋은수열.py
@@ -1,19 +1,28 @@
-def f(s):
-    # 나쁜 수열인지 확인하기
-    l = 1
-    while l <= len(s) // 2:
-        if s[len(s) - l * 2: len(s) - l] == s[len(s) - l: len(s)]:
-            return
-        l += 1
-
-    if len(s) == n:
-        print(s)
-        exit(0)
-
-    f(s + '1')
-    f(s + '2')
-    f(s + '3')
-
-
 n = int(input())
+
+nums = ['1', '2', '3']
+
+
+# 나쁜 수열이면 True
+def is_bad(s):
+    size = len(s)
+    if size < 2:
+        return False
+
+    for chunk in range(1, size // 2 + 1):
+        if s[size - chunk * 2: size - chunk] == s[size - chunk: size]:
+            return True
+    return False
+
+
+def f(ans):
+    if len(ans) == n:
+        print(ans)
+        exit(0)
+    else:
+        for num in nums:
+            if not is_bad(ans + num) and f(ans + num):
+                return True
+        return False
+
 f('')
