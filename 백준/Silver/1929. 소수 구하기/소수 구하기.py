8d44bb49216ddcb779b81479db942d2a3d95529c
Juna Choi <jn307742@gmail.com>
2024-10-02 09:31:25 +0900

    [Silver III] Title: 소수 구하기, Time: 248 ms, Memory: 46468 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,13 +1,11 @@
 m, n = map(int, input().split())
 
-is_prime = [True] * (n + 1)
-is_prime[0] = is_prime[1] = False
+is_prime = [False] * 2 + [True] * (n - 1)
 
-for a in range(2, int(n ** 0.5) + 1):
-    if is_prime[a]:
-        for b in range(a + a, n + 1, a):
-            is_prime[b] = False
-
+for x in range(2, int(n ** 0.5) + 1):
+    if is_prime[x]:
+        for y in range(x * 2, n + 1, x):
+            is_prime[y] = False
 
 for x in range(m, n + 1):
     if is_prime[x]:
