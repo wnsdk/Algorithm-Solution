e29fb40fb08b9f19cac2c9858ae4f25663be3ee1
Juna Choi <jn307742@gmail.com>
2024-10-04 14:06:08 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 328 ms, Memory: 53428 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -4,12 +4,15 @@ input = sys.stdin.readline
 
 
 def union(x, y):
-    x = p[x]
-    y = p[y]
-    if x > y:
-        p[x] = y
-    else:
-        p[y] = x
+    x = find(x)
+    y = find(y)
+
+    # x와 y를 합칠 수 없음 (사이클 발생)
+    if x == y:
+        return False
+
+    p[x] = y
+    return True
 
 
 def find(x):
@@ -22,29 +25,26 @@ def kruskal():
     ans = 0     # MST의 가중치 합
     cnt = 0     # 선택된 간선의 개수
 
-    while cnt < V and h:
+    while cnt < V - 1:
         # 최소 비용 간선 선택하기
         e, v1, v2 = heapq.heappop(h)
 
         # 사이클 발생하는지 확인하기
-        if find(v1) == find(v2):
-            continue
-
-        union(v1, v2)
-        ans += e
-        cnt += 1
+        if union(v1, v2):
+            ans += e
+            cnt += 1
 
     return ans
 
 
 V, E = map(int, input().split())
 
+visited = [False] * (V + 1)
 p = [i for i in range(V + 1)]  # 루트 노드
 h = []  # 최소 힙
-visited = [False] * (V + 1)
 
 for _ in range(E):
     a, b, c = map(int, input().split())
-    heapq.heappush(h, (c, a, b))
+    heapq.heappush(h, [c, a, b])
 
 print(kruskal())
