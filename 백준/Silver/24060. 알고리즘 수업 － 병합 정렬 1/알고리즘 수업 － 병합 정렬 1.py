e51e587c08ce12805d6bed892f504a4755d7fe20
Juna Choi <jn307742@gmail.com>
2024-10-04 12:55:12 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 1968 ms, Memory: 89736 KB -BaekjoonHub

--- 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.py
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.py
@@ -1,12 +1,4 @@
-ans = -1
 cnt = 0
-
-
-def chk_ans(x):
-    global cnt, ans
-    cnt += 1
-    if cnt == k:
-        ans = x
 
 
 def merge_sort(A, l, r):
@@ -18,7 +10,7 @@ def merge_sort(A, l, r):
 
 
 def merge(A, l, m, r):
-    global ans, cnt
+    global cnt
 
     i = l
     j = m + 1
@@ -26,26 +18,22 @@ def merge(A, l, m, r):
     tmp = [0] * (r - l + 1)
     while i <= m and j <= r:
         if A[i] <= A[j]:
-            chk_ans(A[i])
             tmp[t] = A[i]
             t += 1
             i += 1
         else:
-            chk_ans(A[j])
             tmp[t] = A[j]
             t += 1
             j += 1
 
     # 왼쪽 배열이 남았다면
     while i <= m:
-        chk_ans(A[i])
         tmp[t] = A[i]
         t += 1
         i += 1
 
     # 오른쪽 배열이 남았다면
     while j <= r:
-        chk_ans(A[j])
         tmp[t] = A[j]
         t += 1
         j += 1
@@ -53,6 +41,10 @@ def merge(A, l, m, r):
     i = l
     t = 0
     while i <= r:
+        cnt += 1
+        if cnt == k:
+            print(tmp[t])
+            exit(0)
         A[i] = tmp[t]
         i += 1
         t += 1
@@ -62,4 +54,4 @@ n, k = map(int, input().split())
 a = list(map(int, input().split()))
 
 merge_sort(a, 0, len(a) - 1)
-print(ans)
+print(-1)
